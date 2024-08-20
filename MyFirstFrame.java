675e4138b73782e1cd661ec9e9470e1b7db4b486
pathummadhusanka <pathum23200@gmail.com>
2024-08-21 00:23:37 +0530

    Question 04

---
+++ MyFirstFrame.java
@@ -0,0 +1,14 @@
+// Question 04
+
+import javax.swing.*;
+
+public class MyFirstFrame {
+    
+    public static void main(String[] args) {
+        JFrame myWindow = new JFrame();
+        myWindow.setSize(300, 200); // Window size
+        myWindow.setTitle("My First Frame"); // Window title
+        myWindow.setLocation(100, 50); // Set position 100px from screen left and 50px from screen top
+        myWindow.setVisible(true); // Set visible
+    }
+}
