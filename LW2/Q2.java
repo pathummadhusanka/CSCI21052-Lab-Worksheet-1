41fe64c115ef43b14814a61d188185efc5fb96fe
pathummadhusanka <pathum23200@gmail.com>
2024-10-02 19:54:20 +0530

    LW2 Q2

---
+++ LW2/Q2.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Q2 {
+
+  // Method for return the number of digits
+  public static int digitCount(int num) {
+    return Integer.toString(num).length();
+  } 
+  public static void main(String[] args) {
+    
+    Scanner myScanner = new Scanner(System.in);
+
+    while(true) {
+
+      // Read integer from user
+      System.out.print("Enter number: ");
+      int num = myScanner.nextInt();
+
+      // Jump out from loop id number is negative
+      if(num < 0) {
+        System.out.println("Finished!");
+        break;
+      }else {
+        System.out.println("Digit count: " + digitCount(num));
+        System.out.println();
+      }
+    }
+
+
+
+    
+  }
+}
